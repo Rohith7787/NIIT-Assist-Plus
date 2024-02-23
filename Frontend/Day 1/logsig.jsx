85d740c531d8cdb7aa73ee099de976824e4d8efe
Rohith7787 <119097024+Rohith7787@users.noreply.github.com>
2024-02-23 14:34:29 +0530

    Add files via upload

---
+++ Frontend/Day 1/logsig.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './logsig.css'; // Import your CSS file
+import { Route } from 'react-router-dom';
+
+function SignUpForm({ setActivePanel }) {
+  return (
+    <div className="form-container sign-up-container">
+      <form>
+        <h2>User Signup</h2>
+        <input placeholder="Username" />
+        <input placeholder="Email" />
+        <input placeholder="Password" />
+        
+        <button onClick={() => setActivePanel('signIn')}>Register</button>
+      </form>
+    </div>
+  );
+}
+
+function SignInForm({ setActivePanel }) {
+  return (
+    <div className="form-container sign-in-container">
+      <form>
+        <h2>User Login</h2>
+        <input placeholder="Username" />
+        <input placeholder="Password" />
+        
+        <button onClick={() => setActivePanel('signUp')}>Login</button>
+      </form>
+    </div>
+  );
+}
+
+function AuthForm() {
+  const [activePanel, setActivePanel] = useState('signIn');
+
+  return (
+    <div className={`container ${activePanel === 'signIn' ? '' : 'right-panel-active'}`} id="container">
+      <SignUpForm setActivePanel={setActivePanel} />
+      <SignInForm setActivePanel={setActivePanel} />
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="bg-bubbles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </div>
+          <div className="overlay-panel overlay-left">
+            <h1 id='t2'>Welcome Back!</h1>
+            <p>Enhance skills that meet your need online</p>
+            <button className="ghost" id="signIn" onClick={() => setActivePanel('signIn')}>Sign In</button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1 id='t2'>Hello, Friend!</h1>
+            <p>Enter your personal details and start the journey with us</p>
+            <button className="ghost" id="signUp" onClick={() => setActivePanel('signUp')}>Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuthForm;
